764220f0b0a9bc6ed6dd89de13dcfc1ac541892e
Abdelhamied Mostafa <Abdelhamied200@gmail.com>
2021-02-27 15:15:00 +0200

    axios prod base url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+let url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:9000/api"
+    : "https://whatsapapi.herokuapp.com/api";
+
 const instance = axios.create({
-  baseURL: "http://localhost:9000/api",
+  baseURL: url,
 });
 
 export default instance;
